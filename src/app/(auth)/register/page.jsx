c90844b6bd9d0c7b95163d40abb6402a13e7dfcb
Mehedi Hasan Munna <mehedihasanmunna516@gmail.com>
2025-12-17 18:33:57 +0600

    login page updated

--- src/app/(auth)/register/page.jsx
+++
@@ -1,7 +0,0 @@
-export default function Register() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold">This is the register page</h1>
-    </div>
-  );
-}
